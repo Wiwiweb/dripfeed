27b0eca73312ff08921a40c2d839c4b6c6040a25
Wiwiweb <wiwiweb2@gmail.com>
2016-10-16 18:01:03 -0700

    Barebones rss generator and route

---
+++ routes/feed.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+var RSSGenerator = require('../lib/RSSGenerator');
+
+router.get('/', function(req, res, next) {
+    var startDate = req.query.startDate;
+    var frequency = req.query.frequency;
+    if (startDate == null || frequency == null) {
+        return res.status(400).send('Missing parameters').end();
+    }
+
+    startDate = new Date(startDate);
+    frequency = parseInt(frequency);
+    if (!startDate instanceof Date || isNaN(frequency)) {
+        return res.status(400).send('Invalid parameters').end();
+    }
+
+    var feed = RSSGenerator.createFeed(startDate, frequency);
+    res.set('Content-Type', 'application/rss+xml');
+    res.send(feed.xml({indent: true}));
+});
+
+module.exports = router;

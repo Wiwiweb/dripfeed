24682f0f64e7bf3f549ad921f001c2c041979184
Wiwiweb <wiwiweb2@gmail.com>
2016-12-17 16:14:23 -0800

    Add extra test data

--- sql/setup_test_data.sql
+++ sql/setup_test_data.sql
@@ -11,4 +11,6 @@ INSERT INTO pages (page_nb, url, title, webcomic_id)
 VALUES
 ('0', 'http://xkcd.com/1/', 'Barrel - Part 1', (SELECT id from webcomics WHERE name='XKCD')),
 ('1', 'http://xkcd.com/2/', 'Petit Trees (sketch)', (SELECT id from webcomics WHERE name='XKCD')),
-('2', 'http://xkcd.com/3/', 'Island (sketch)', (SELECT id from webcomics WHERE name='XKCD'));
+('2', 'http://xkcd.com/3/', 'Island (sketch)', (SELECT id from webcomics WHERE name='XKCD')),
+('3', 'http://xkcd.com/4/', 'Landscape (sketch)', (SELECT id from webcomics WHERE name='XKCD')),
+('4', 'http://xkcd.com/5/', 'Blown apart', (SELECT id from webcomics WHERE name='XKCD'));

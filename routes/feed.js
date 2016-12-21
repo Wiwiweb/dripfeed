2493026cd141c5fc631c327016f7e02987c2ae0a
Wiwiweb <wiwiweb2@gmail.com>
2016-12-20 22:36:43 -0800

    Catch errors during feed route

--- routes/feed.js
+++ routes/feed.js
@@ -17,9 +17,13 @@ router.get('/', function(req, res, next) {
     }
 
     var rssGenerator = new RSSGenerator(0, startDate, frequency);
-    rssGenerator.createFeed().then(feed => {
+    rssGenerator.createFeed()
+    .then(feed => {
         res.set('Content-Type', 'application/rss+xml');
         res.send(feed.xml({indent: true}));
+    })
+    .catch(err => {
+        next(err);
     });
 });
 

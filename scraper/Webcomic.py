24061b4ce87457248e59732eba779fb2e83bfb00
Wiwiweb <wiwiweb2@gmail.com>
2017-01-26 18:08:21 -0800

    Add first db commands to the scraper

--- scraper/Webcomic.py
+++ scraper/Webcomic.py
@@ -1,18 +1,33 @@
 from abc import ABC, abstractmethod
+import os
 import logging
+import psycopg2
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+
+db_url = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
+debug = os.environ['NODE_ENV']
+if db_url is None:
+    db_url = 'postgresql://postgres:postgres@127.0.0.1:5432'
+
+if debug:
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    db_name = 'testing'
+else:
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(message)s')
+    db_name = 'dripfeed'
 
 
 class Webcomic(ABC):
     def __init__(self):
         self.current_page = 0
         self.current_url = None
+        self.webcomic_id = 0  # TODO
 
     def get_and_process_next_page(self):
         self.get_next_page_and_update_state()
-        self.add_current_page_to_db()
+        if self.current_page > 0:
+            self.add_current_page_to_db()
 
     def get_next_page_and_update_state(self):
         next_page_url = self.get_next_page_url()
@@ -29,5 +44,9 @@ class Webcomic(ABC):
         pass
 
     def add_current_page_to_db(self):
-        # TODO
-        pass
+        with psycopg2.connect("{}/{}".format(db_url, db_name)) as connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO webcomics (page_nb, url, title, webcomic_id)" + \
+                      "VALUES ((%s), (%s), (%s), (%s))"
+                values = [self.current_page, self.current_url, "", self.webcomic_id]
+                cursor.execute(sql, values)

2feb0d456e342bb579837bed25eec9c62c2ad33d
Wiwiweb <wiwiweb2@gmail.com>
2017-03-04 19:41:26 -0800

    Add basic front page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 let express = require('express');
 let router = express.Router();
 
+let db = require('../lib/db');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    getAllWebcomics()
+    .then(webcomics =>
+        res.render('index', {
+            webcomics: webcomics,
+            timeNow: getCurrentDate()
+        }));
+
 });
+
+
+function getAllWebcomics() {
+    return db("SELECT * FROM webcomics")
+    .then(result => result.rows)
+}
+
+function getCurrentDate() {
+    let date = new Date();
+    return date.toISOString().split('.')[0]+"Z";
+
+}
 
 module.exports = router;

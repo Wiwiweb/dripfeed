00ef5d9ec02119b4e92069b7d1776a406a23b701
Wiwiweb <wiwiweb2@gmail.com>
2017-04-27 11:00:38 -0700

    DB url changes for OpenShift
    Turns out the full URL env var already has a slash

--- scraper/db.py
+++ scraper/db.py
@@ -5,7 +5,7 @@ FIXTURES_FILE = '../sql/test_fixtures.sql'
 
 db_url = os.getenv('OPENSHIFT_POSTGRESQL_DB_URL')
 if db_url is None:
-    db_url = 'postgresql://postgres:postgres@127.0.0.1:5432'
+    db_url = 'postgresql://postgres:postgres@127.0.0.1:5432/'
 
 debug = os.getenv('NODE_ENV') != 'production'
 if debug:
@@ -15,7 +15,7 @@ else:
 
 
 def query(sql, values=None):
-    with psycopg2.connect("{}/{}".format(db_url, db_name)) as connection:
+    with psycopg2.connect("{}{}".format(db_url, db_name)) as connection:
         with connection.cursor() as cursor:
             cursor.execute(sql, values)
             try:

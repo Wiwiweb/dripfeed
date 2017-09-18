3779e673bf9131831ff080c0648414f6e7a4f183
Wiwiweb <wiwiweb2@gmail.com>
2017-09-17 20:17:06 -0700

    Tiny refactoring (== -> ===)

--- routes/feed.js
+++ routes/feed.js
@@ -8,19 +8,19 @@ router.get('/', function(req, res, next) {
     let startDate = req.query.startDate;
     let frequency = req.query.frequency;
     let startPage = req.query.startPage;
-    if (webcomicId == null || startDate == null || frequency == null) {
+    if (webcomicId === null || startDate === null || frequency === null) {
         return res.status(400).send('Missing parameters').end();
     }
 
     startDate = new Date(startDate);
     webcomicId = parseInt(webcomicId);
     frequency = parseInt(frequency);
-    if (startPage != null) {
+    if (startPage !== null) {
         startPage = parseInt(startPage);
     } else {
         startPage = undefined;
     }
-    if (!isValidDate(startDate) || isNaN(webcomicId) || isNaN(frequency) || (startPage != undefined && startPage < 1)) {
+    if (!isValidDate(startDate) || isNaN(webcomicId) || isNaN(frequency) || (startPage !== undefined && startPage < 1)) {
         return res.status(400).send('Invalid parameters').end();
     }
 
